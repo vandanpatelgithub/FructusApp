0f8c97316ec9e3d02ea834f159d28cb788ff14a7
Vandan Patel <vandan.ets@gmail.com>
2021-02-13 22:53:00 -0500

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Vandan Patel on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
